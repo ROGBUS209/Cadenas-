fffce60b32d775c9355e7716c2a0de961086e9e4
Berenice <Berenice@Berenice-PC>
2021-01-11 23:37:28 -0600

    11/01/2021

---
+++ Cadena.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+
+    char A[6] = {'H','e','l','l','o',0};
+     A[1];
+     
+   
+    printf("%c", A[1]);
+    
+        
+
+    return (EXIT_SUCCESS);
+}
+
